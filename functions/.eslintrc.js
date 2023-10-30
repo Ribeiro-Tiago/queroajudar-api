cfe029e9c7afa2984f5688c7721487332b5268e7
Tiago Ribeiro <t.ribeiro@sweepatic.com>
2023-10-30 17:26:37 +0000

    linting fix

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,19 +14,20 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    tsconfigRootDir: __dirname,
     project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    "quote-props": "off",
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    indent: ["error", 2],
+    "object-curly-spacing": "off",
+    "max-len": ["error", 120],
   },
 };
