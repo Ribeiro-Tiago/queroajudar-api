fa239a420ba2c90e4e885d0e03a3ada0155954b3
Tiago Ribeiro <t.ribeiro@sweepatic.com>
2023-11-08 15:09:14 +0000

    linting tweak

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -32,5 +32,6 @@ module.exports = {
     "require-jsdoc": "off",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
   },
 };
