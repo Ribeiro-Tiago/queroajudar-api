972d0514034dd56c148749ae202a0de177f8fd3d
Tiago Ribeiro <t.ribeiro@sweepatic.com>
2023-11-08 17:52:28 +0000

    update error handling

--- functions/src/middleware/index.ts
+++ functions/src/middleware/index.ts
@@ -1,10 +1,18 @@
+import { ValidationError } from "joi";
 import { Request } from "firebase-functions/v2/https";
 import { Response } from "express";
 
 import { HttpsFunctionHandler, GenericObject } from "../types";
 
 export const applyMiddleware = (handler: HttpsFunctionHandler) => (req: Request, res: Response) => {
-  res.validationError = (errors: GenericObject, reason = "Validation error") => {
+  res.validationError = (errors: ValidationError | GenericObject, reason = "Validation error") => {
+    if (errors instanceof ValidationError) {
+      errors = errors.details.reduce((result, { message, path }) => {
+        result[path[0]] = message;
+        return result;
+      }, {});
+    }
+
     res.status(422).json({ reason, errors });
   };
 
